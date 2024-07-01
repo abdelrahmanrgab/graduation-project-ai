07d66b541d786ed0ff21125e2cd8ff206cfe8e3a
Ahmed-bahget <ahedbahget70@gmail.com>
2024-07-01 05:04:04 +0300

    last code which we will work on him

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def generate_schema(user_input: UserInput):
 # Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
